d4e3a0cd0b95a18df9936bcde632f6bd20c51ac7
mikhapolishchuk-code <mikha.polishchuk@gmail.com>
2025-12-04 21:16:48 +0000

    Fix reaction time calculation and improve LED logic

--- PCB_Project.ino
+++ PCB_Project.ino
@@ -6,7 +6,6 @@ int LED1 = D3;
 int LED2 = D4;
 int LED3 = D5;
 
-int currentLED = 0;
 int LEDs [] = {LED1, LED2, LED3};
 int BUTs [] = {BUT1, BUT2, BUT3};
 
@@ -25,32 +24,42 @@ void setup() {
 
 void loop() {
   
-  int startTime = milis();
   int endTime;
   int reactionTime;
 
-  randomNum = random(0, 3);
+  int randomNum = random(0, 3);                     //picks a random number in order to light up that LED
 
   digitalWrite(LEDs[randomNum], HIGH);
+  int startTime = millis();                      //gets the time from when the LED is lit
 
-  while (BUTs[randomNum] != LOW)
+  while (digitalRead(BUTs[randomNum]) != LOW)
   {
-      endTime = millis();
+      endTime = millis();                                  //updates the time until you press the right button
   }
   
-  digitalWrite(LEDs[randomNum], LOW);
-  reactionTime = endTime - startTime;
+  digitalWrite(LEDs[randomNum], LOW);                       //turns off LED
+  reactionTime = endTime - startTime;                          //calculates reaction time
 
-  if (endtime <= 200)
+  if (reactionTime <= 200)                                 //if your reaction time is fast enough the lights will turn on and off repeatedly 
   {
-    
-  }
-    
-    
+    for(int i = 0; i<3; i++)
+      {
+        digitalWrite(LED1, HIGH);
+        digitalWrite(LED2, HIGH);
+        digitalWrite(LED3, HIGH);
 
-  
-  
-  
+        delay(200);
+
+        digitalWrite(LED1, LOW);
+        digitalWrite(LED2, LOW);
+        digitalWrite(LED3, LOW);
+
+        delay(200);
+        
+      }
+  }
+
+  delay(random(500, 1000));                                    //random delay before the next round so you cant always know when you need to press the button
 }
 
 
